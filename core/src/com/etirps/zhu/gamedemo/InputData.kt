cd6b4498bc5ec49b4ec06a5241349f9064cce307
Andrew Lee <andrew1421lee@gmail.com>
2019-02-17 22:22:22 -0500

    new coordinates and flags

--- core/src/com/etirps/zhu/gamedemo/InputData.kt
+++ core/src/com/etirps/zhu/gamedemo/InputData.kt
@@ -1,3 +1,5 @@
 package com.etirps.zhu.gamedemo
 
-data class InputData(var origX: Float = 0f, var origY: Float = 0f, var touchDown: Boolean = false)
+data class InputData(var origX: Float = 0f,             var origY: Float = 0f,
+                     var destX: Float = 0f,             var destY: Float = 0f,
+                     var touchedDown: Boolean = false,  var dragging: Boolean = false)
