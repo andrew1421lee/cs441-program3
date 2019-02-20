44c467be39e07647786100c78e343df37005a8c9
Andrew Lee <andrew1421lee@gmail.com>
2019-02-20 00:44:21 -0500

    comments and cleaning

--- core/src/com/etirps/zhu/gamedemo/InputData.kt
+++ core/src/com/etirps/zhu/gamedemo/InputData.kt
@@ -4,18 +4,20 @@ import kotlin.math.*
 
 data class InputData(var origX: Float = 0f,             var origY: Float = 0f,
                      var destX: Float = 0f,             var destY: Float = 0f,
-                     var touchedDown: Boolean = false,  var dragging: Boolean = false)
+                     var touchedDown: Boolean = false,  var dragging: Boolean = false,
+                     var touchedUp: Boolean = false)
 {
+    /**
+     * Returns the distance between orig points and dest points
+     */
     val distance: Float
         get() {
             return sqrt((destX - origX).pow(2) + (destY-origY).pow(2))
         }
 
-    val powerLineDistance: Float
-        get() {
-            return sqrt((powerLineX - origX).pow(2) + (powerLineY-origY).pow(2))
-        }
-
+    /**
+     * Returns the angle between the two points
+     */
     val angle: Float
         get() {
             val opposite = origX.toDouble() - destX.toDouble()
@@ -28,19 +30,31 @@ data class InputData(var origX: Float = 0f,             var origY: Float = 0f,
             return -angle.toFloat()
         }
 
-    val powerLineAngle: Float
+    /**
+     * Returns the distance of the aiming line
+     */
+    val aimingDistance: Float
         get() {
-            val opposite = powerLineX.toDouble() - origX.toDouble()
-            val adjacent = powerLineY.toDouble() - origY.toDouble()
+            return sqrt((aimingX - origX).pow(2) + (aimingY-origY).pow(2))
+        }
+
+    /**
+     * Returns the angle of the aiming line
+     */
+    val aimingAngle: Float
+        get() {
+            val opposite = aimingX.toDouble() - origX.toDouble()
+            val adjacent = aimingY.toDouble() - origY.toDouble()
 
             val angle = atan2(opposite, adjacent)
-
-            //angle += ceil(-angle / 360) * 360
 
             return angle.toFloat()
         }
 
-    val powerLineX: Float
+    /**
+     * Returns the end point (x) of the aiming line
+     */
+    val aimingX: Float
         get() {
             return if(distance < 400) {
                 destX
@@ -50,7 +64,10 @@ data class InputData(var origX: Float = 0f,             var origY: Float = 0f,
             }
         }
 
-    val powerLineY: Float
+    /**
+     * Returns the end point (y) of the aiming line
+     */
+    val aimingY: Float
         get() {
             return if(distance < 400) {
                 destY
