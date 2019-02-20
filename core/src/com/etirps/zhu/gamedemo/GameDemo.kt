6b63bf87a61f88df8c31f29f692c2aaf16247015
Andrew Lee <andrew1421lee@gmail.com>
2019-02-20 00:44:57 -0500

    comments, cleaning, and removing spaghetti

--- core/src/com/etirps/zhu/gamedemo/GameDemo.kt
+++ core/src/com/etirps/zhu/gamedemo/GameDemo.kt
@@ -72,9 +72,6 @@ class GameDemo : ApplicationAdapter(), InputProcessor {
         hud = fontGenerator.generateFont(fontParameters)
         fontGenerator.dispose()
 
-        //hud.data.scale(2f)
-        //hud.region.texture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear)
-
         // ENABLE OR DISABLE DEBUG MODE
         //debugFont = BitmapFont()
 
@@ -84,9 +81,7 @@ class GameDemo : ApplicationAdapter(), InputProcessor {
         bulletImg = Texture("bullet.png")
         playerImg = Texture("player.png")
 
-
         // Initialize game objects
-
         rocks = mutableListOf()
         bullets = mutableListOf()
 
@@ -98,14 +93,11 @@ class GameDemo : ApplicationAdapter(), InputProcessor {
         coolDown = 500
         player = Player(Gdx.graphics.width.toFloat() / 2, Gdx.graphics.height.toFloat() / 2, playerImg, debugFont = debugFont)
         stage.addActor(player)
-        spawnRock()
-        spawnRock()
-        spawnRock()
-        spawnRock()
-        spawnRock()
+        spawnRock(5)
     }
 
     private fun clearStage() {
+        // Remove all actors from the stage
         for (rock in rocks) {
             rock.remove()
         }
@@ -119,89 +111,116 @@ class GameDemo : ApplicationAdapter(), InputProcessor {
         player.remove()
     }
 
-    private fun spawnRock() {
-        // Get x y positions for new rock
-        val newPosX = (0..Gdx.graphics.width).random()
-        val newPosY = (0..Gdx.graphics.height).random()
+    private fun spawnRock(numberOfRocks: Int) {
+        for (i in 1..numberOfRocks){
+            // Get x y positions for new rock
+            val newPosX = (0..Gdx.graphics.width).random()
+            val newPosY = (0..Gdx.graphics.height).random()
 
-        // Get random speeds for x, y, and spin
-        var newSpeedX = (Random.nextInt(from = 10, until = 20).toFloat() / 10f) * (-1..1).random()
-        if(newSpeedX == 0f) { newSpeedX = 1f }
+            // Get random speeds for x, y, and spin
+            var newSpeedX = (Random.nextInt(from = 10, until = 20).toFloat() / 10f) * (-1..1).random()
+            if(newSpeedX == 0f) { newSpeedX = 1f }
 
-        var newSpeedY = (Random.nextInt(from = 10, until = 20).toFloat() / 10f) * (-1..1).random()
-        if(newSpeedY == 0f) { newSpeedY = 1f }
+            var newSpeedY = (Random.nextInt(from = 10, until = 20).toFloat() / 10f) * (-1..1).random()
+            if(newSpeedY == 0f) { newSpeedY = 1f }
 
-        var newSpeedSpin = Random.nextInt(from = 10, until = 20).toFloat() / 75f * (-1..1).random()
-        if(newSpeedSpin == 0f) { newSpeedSpin = 10f / 75f }
+            var newSpeedSpin = Random.nextInt(from = 10, until = 20).toFloat() / 75f * (-1..1).random()
+            if(newSpeedSpin == 0f) { newSpeedSpin = 10f / 75f }
 
-        // Create new rock
-        val rock = Rock(newPosX.toFloat(), newPosY.toFloat(), rockImg, speedX = newSpeedX, speedY = newSpeedY, speedSpin = newSpeedSpin, debugFont = debugFont)
+            // Create new rock
+            val rock = Rock(newPosX.toFloat(), newPosY.toFloat(), rockImg, speedX = newSpeedX, speedY = newSpeedY, speedSpin = newSpeedSpin, debugFont = debugFont)
 
-        // Add rock to stage
-        rocks.add(rock)
-        stage.addActor(rock)
+            // Add rock to stage
+            rocks.add(rock)
+            stage.addActor(rock)
+        }
     }
 
     private fun checkCollision() {
 
+        // Create lists for removed rocks, bullets, and created rocks
         val deadBullets = mutableListOf<Bullet>()
         val deadRocks = mutableListOf<Rock>()
         val newRocks = mutableListOf<Rock>()
 
+        // Check if the player hit anything
         for(rock in rocks) {
             if(player.shield > 0 || gameOver) { break }
 
+            // Player collided with rock
             if(Intersector.overlapConvexPolygons(player.polygon, rock.polygon)) {
+                // Set game over flag
                 gameOver = true
+
+                // Split the rock, add it to new rocks list
                 newRocks.addAll(rock.split())
+                // Remove the rock from the stage and add it to dead rocks list
                 rock.remove()
                 deadRocks.add(rock)
 
+                // Create explosion
                 stage.addActor(Explosion(player.x - player.width / 2, player.y - player.width / 2, player.width, explosionImg, 2000f, debugFont =  debugFont))
                 stage.addActor(Explosion(player.x - player.width * 2f / 2, player.y - player.width * 2f / 2, player.width * 2f, explosionImg, 1000f, debugFont))
                 stage.addActor(Explosion(player.x - player.width * 3f / 2, player.y - player.width * 3f / 2, player.width * 3f, explosionImg, 500f, debugFont))
 
+                // Remove player
                 player.remove()
             }
         }
 
+        // Remove all rocks so removed rocks aren't used for next step
         rocks.removeAll(deadRocks)
+        deadRocks.clear()
 
+        // For every bullet on screen
         for(bullet in bullets) {
+            // If, for some reason, bullet is not active, remove it and go next
             if(!bullet.active) {
                 deadBullets.add(bullet)
                 continue
             }
 
+            // For every rock on screen
             for(rock in rocks) {
+                // If the bullet and rock collided
                 if(Intersector.overlapConvexPolygons(bullet.polygon, rock.polygon)) {
+                    // Split the rock and add the result
                     newRocks.addAll(rock.split())
 
+                    // Remove the bullet and rock
                     bullet.remove()
                     rock.remove()
 
+                    // Add them to be removed later
                     deadRocks.add(rock)
                     deadBullets.add(bullet)
 
+                    // Add explosion
                     stage.addActor(Explosion(rock.x - rock.size * 0.5f / 2, rock.y - rock.size * 0.5f / 2, rock.size * 0.5f, explosionImg, 1000f, debugFont = debugFont))
                     stage.addActor(Explosion(rock.x - rock.size / 2, rock.y - rock.size / 2, rock.size, explosionImg, debugFont = debugFont))
 
+                    // Update the score
                     player.score += 250 - rock.size.toInt()
                 }
             }
         }
 
+        // Remove all resulting dead rocks and bullets
         bullets.removeAll(deadBullets)
         rocks.removeAll(deadRocks)
 
+        // Add all new rocks
         for(newRock in newRocks) {
             stage.addActor(newRock)
         }
-
         rocks.addAll(newRocks)
+
     }
 
-    private fun actOnInput() {
+    private fun drawAimingReticule() {
+        shapes.begin(ShapeRenderer.ShapeType.Filled)
+        shapes.color = Color.WHITE
+
         if(input.touchedDown) {
             // Draw a circle at initial touch point
             shapes.circle(player.x + player.height / 2, player.y + player.height /2, 10f)
@@ -210,33 +229,40 @@ class GameDemo : ApplicationAdapter(), InputProcessor {
         if(input.dragging) {
             // Draw a line to where finger is currently
             shapes.rectLine(Vector2(player.x + player.height / 2, player.y + player.height /2),
-                    Vector2(player.x + player.height / 2 - (input.origX - input.powerLineX),
-                            player.y + player.height /2 - (input.origY - input.powerLineY)), 10f)
+                    Vector2(player.x + player.height / 2 - (input.origX - input.aimingX),
+                            player.y + player.height /2 - (input.origY - input.aimingY)), 10f)
 
-            shapes.rectLine(Vector2(player.x + player.height / 2 - (input.origX - input.powerLineX),
-                    player.y + player.height /2 - (input.origY - input.powerLineY)),
+            shapes.rectLine(Vector2(player.x + player.height / 2 - (input.origX - input.aimingX),
+                    player.y + player.height /2 - (input.origY - input.aimingY)),
                     Vector2(player.x + player.height / 2 - (input.origX - input.destX),
                             player.y + player.height /2 - (input.origY - input.destY)), 3f)
 
-            player.rotation = input.angle
         }
+
+        shapes.end()
     }
 
     private fun updateHUD() {
+        // Get values to display on HUD
         //val speed = String.format("%.1f", sqrt((player.x + player.speedX - player.x).pow(2) + (player.y + player.speedY - player.y).pow(2)))
         val speedX = String.format("%.1f", player.speedX)
         val speedY= String.format("%.1f", player.speedY)
 
+        // Start drawing the hud
         batch.begin()
+        // Draw the score display
         hud.draw(batch, "SCORE: ${player.score}", Gdx.graphics.width - 400f, Gdx.graphics.height - 50f)
+        // Draw speedometers
         hud.draw(batch, "SPEED X: $speedX", Gdx.graphics.width / 2 - 400f, 75f)
         hud.draw(batch, "SPEED Y: $speedY", Gdx.graphics.width / 2 + 100f, 75f)
 
+        // Check for status effects
         if(player.shield > 0) {
             hud.draw(batch, "INVINCIBLE", Gdx.graphics.width / 2f - 100f, Gdx.graphics.height / 2f)
             hud.draw(batch, "${player.shield}", Gdx.graphics.width / 2f - 5f, Gdx.graphics.height / 2f - 100f)
         }
 
+        // Check for game over
         if(gameOver) {
             hud.draw(batch, "GAME OVER", Gdx.graphics.width / 2f - 100f, Gdx.graphics.height / 2f)
             if(coolDown > 0) {
@@ -248,6 +274,7 @@ class GameDemo : ApplicationAdapter(), InputProcessor {
 
         }
 
+        // Finish drawing
         batch.end()
     }
 
@@ -267,24 +294,20 @@ class GameDemo : ApplicationAdapter(), InputProcessor {
         fpsCounter.update()
         fpsCounter.render()
 
+        player.rotation = input.angle
+
         // Update all actors in stage
         stage.act(Gdx.graphics.deltaTime)
 
         // Draw stage actors
         stage.draw()
 
-        // Draw input shapes
-        shapes.begin(ShapeRenderer.ShapeType.Filled)
-        shapes.color = Color.WHITE
-
+        // Draw aiming reticule
         if(!gameOver) {
-            actOnInput()
+            drawAimingReticule()
         }
 
         updateHUD()
-
-        shapes.end()
-
         checkCollision()
 
     }
@@ -304,8 +327,8 @@ class GameDemo : ApplicationAdapter(), InputProcessor {
             return true
         }
         // Discover what direction the bullet should be firing
-        val adjacent = cos(input.powerLineAngle) * input.powerLineDistance
-        val opposite = sin(input.powerLineAngle) * input.powerLineDistance
+        val adjacent = cos(input.aimingAngle) * input.aimingDistance
+        val opposite = sin(input.aimingAngle) * input.aimingDistance
 
         // Create new bullet
         val bullet = Bullet(player.x + (player.width / 2), player.y + (player.height / 2), player.rotation, Vector2(opposite / 30f, adjacent / 30f), bulletImg, debugFont)
@@ -327,7 +350,7 @@ class GameDemo : ApplicationAdapter(), InputProcessor {
         // Convert screen touch coordinates to game coordinates
         val actualXY = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
 
-        // Do nothing if no distance was dragged
+        // Do not update if no distance was dragged
         if(input.origX - actualXY.x == 0f && input.origY - actualXY.y == 0f) {
             input.dragging = false
             return true
