09d7a06bf929386f0d086fbf00f8bb33f75bfe59
Andrew Lee <andrew1421lee@gmail.com>
2019-02-21 15:33:47 -0500

    new powerup light, change weight powers to multiply instead of apply

--- core/src/com/etirps/zhu/gamedemo/LightEffect.kt
+++ core/src/com/etirps/zhu/gamedemo/LightEffect.kt
@@ -2,19 +2,19 @@ package com.etirps.zhu.gamedemo
 
 import com.badlogic.gdx.scenes.scene2d.Actor
 
-class HeavyEffect(time: Float = 5f): StatusEffect() {
+class LightEffect(time: Float = 5f): StatusEffect() {
 
     var applied: Boolean = false
 
     init {
         duration = time
-        type = StatusTypes.HEAVY
+        type = StatusTypes.LIGHT
     }
 
     override fun applyEffect(actor: Actor) {
 
         if(actor is Player && !applied) {
-            actor.mass *= 4f
+            actor.mass *= 0.25f
             applied = true
         }
 
@@ -22,7 +22,7 @@ class HeavyEffect(time: Float = 5f): StatusEffect() {
 
     override fun remove(actor: Actor) {
         if(actor is Player && applied) {
-            actor.mass *= 0.25f
+            actor.mass *= 4f
         }
     }
 }
