6e2c2fb48e618d6bdcf821ecd33b51a06071e222
Andrew Lee <andrew1421lee@gmail.com>
2019-02-21 14:22:48 -0500

    creates power up actors, needs to be builder to distribute texture

---
+++ core/src/com/etirps/zhu/gamedemo/PowerUpBuilder.kt
@@ -0,0 +1,38 @@
+package com.etirps.zhu.gamedemo
+
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+
+enum class StatusTypes { DEFAULT, INVINCIBLE, ROCK }
+
+data class PowerUpData (val type: StatusTypes, val texture: Texture)
+
+class PowerUpBuilder(debugFont: BitmapFont? = null) {
+    private val powerUpList: MutableList<PowerUpData> = mutableListOf()
+    private val font: BitmapFont? = debugFont
+
+    fun loadPowerUp(powerUpData: PowerUpData) {
+        if(!powerUpList.contains(powerUpData)) {
+            powerUpList.add(powerUpData)
+        }
+    }
+
+    fun createPowerUp(posX: Float, posY: Float, type: StatusTypes): PowerUp? {
+        return when(type) {
+            StatusTypes.INVINCIBLE -> PowerUp(posX, posY, InvincibleEffect(), powerUpList.filter { x -> x.type == type }[0].texture, font)
+            StatusTypes.ROCK -> PowerUp(posX, posY, RockEffect(), powerUpList.filter { x -> x.type == type }[0].texture, font)
+            else -> null
+        }
+    }
+
+    fun createPowerUp(posX: Float, posY: Float): PowerUp? {
+        val randomIndex = (1 until StatusTypes.values().size).random()
+        val type: StatusTypes = StatusTypes.values()[randomIndex]
+
+        return when(type) {
+            StatusTypes.INVINCIBLE -> PowerUp(posX, posY, InvincibleEffect(), powerUpList.filter { x -> x.type == type }[0].texture, font)
+            StatusTypes.ROCK -> PowerUp(posX, posY, RockEffect(), powerUpList.filter { x -> x.type == type }[0].texture, font)
+            else -> null
+        }
+    }
+}
